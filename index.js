2a7fabfb081aba0af1b1e01795b70cd5be4d8529
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-14 17:02:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function currentLine(people) {
     copyline += (i + 1) + ". " + people[i] + ", ";
     copyline = copyline.slice(0, copyline.length - 2)
   return "The line is currently: " + copyline;
+}
 } else if (people.length === 0) {
   return "The line is currently empty."
-}
 }
 }
